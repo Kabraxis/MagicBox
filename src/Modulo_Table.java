5152b0551d80d52306ee15f5e03fe708befd4a67
lynx42 <{46925680}+{Kabraxis}@users.noreply.github.com>
2021-01-04 22:26:57 +0100

    completed Modulo table imported planned projects BinaryGrid and DSA helper

--- src/Modulo_Table.java
+++ src/Modulo_Table.java
@@ -4,41 +4,49 @@ public class Modulo_Table {
         verticalModulo();
     }
 
-    public static void lineBreak() {
+    public static void breaksRows() {
         for (int i = 0; i < 101; i++) {
             System.out.print("=");
         }
         System.out.println();
     }
 
-    public static String spaceBreak() {
+    public static String breaksColumns() {
         return "||";
     }
 
     public static void horizontalModulo() {
         System.out.println('\n' + "Horizontal");
-        lineBreak();
+        breaksRows();
         for (int i = 1; i < 10; i++) {
             for (int j = 1; j < 10; j++) {
-                System.out.print(spaceBreak() + " " + j + "%" + i + " = " + j % i + " ");
+                System.out.print(breaksColumns() + " " + j + "%" + i + " = " + j % i + " ");
             }
-            System.out.print(spaceBreak());
+            System.out.print(breaksColumns());
             System.out.println();
-            lineBreak();
+            breaksRows();
         }
     }
 
     public static void verticalModulo() {
         System.out.println('\n' + "Vertical");
-        lineBreak();
+        breaksRows();
         for (int i = 1; i < 10; i++) {
             for (int j = 1; j < 10; j++) {
-                System.out.print(spaceBreak() + " " + i + "%" + j + " = " + i % j + " ");
+                System.out.print(breaksColumns() + " " + i + "%" + j + " = " + i % j + " ");
             }
-            System.out.print(spaceBreak());
+            System.out.print(breaksColumns());
             System.out.println();
-            lineBreak();
+            breaksRows();
         }
     }
 }
+/*
+At table to see what patterns Modulo is following.
+Note:
+1. If a / n equals a whole number then a % n always equals 0.
+2. If a is less than n then a % n always equals a.
+3. If a is greater than n then a % n always equals a value between 1 and n - 1
 
+
+ */
